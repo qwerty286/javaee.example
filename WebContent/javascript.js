6740649e4ab216090e70cfba072aa91d563a98c1
Rafał Pasieczyński <31695443+qwerty286@users.noreply.github.com>
2018-03-14 19:18:53 +0100

    Add files via upload

---
+++ WebContent/javascript.js
@@ -0,0 +1,39 @@
+// js scripts source file
+
+
+function changeColor() {
+	var color;
+	color = document.getElementById("main");
+	color.style.backgroundColor = "#79baef";	
+}
+
+function changeOut() {
+	var color;
+	color = document.getElementById("main");
+	color.style.backgroundColor = "#4BA2EA";	
+	
+}
+
+
+function noopacity() {
+	var color;
+	color = document.getElementById("header");
+	color.style.opacity = "1";	
+}
+
+function opacity() {
+	var color;
+	color = document.getElementById("header");
+	color.style.opacity = "0.6";		
+	
+}
+
+function futer() {
+var element = document.querySelector('#foot');
+element.style.color = "red";
+}
+
+
+
+
+
