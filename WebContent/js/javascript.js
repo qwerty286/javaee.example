8e9816486ba6e35fc9f2bc2f29f334faf41bb457
Rafał Pasieczyński <31695443+qwerty286@users.noreply.github.com>
2018-03-19 20:26:27 +0100

    added extension to info about links, new web style

---
+++ WebContent/js/javascript.js
@@ -0,0 +1,20 @@
+// js scripts source file
+
+console.log("go js script");
+
+
+
+
+
+
+function futer() {
+var element = document.querySelector('#foot');
+element.style.color = "red";
+}
+
+
+
+
+
+
+
